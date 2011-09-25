6f720f977598fae9ef865db0836acf0810852725
RJ Salicco <rj.salicco@axiomaticit.com>
2011-09-25 14:38:36 -0400

    Added ${app.name} to URL path.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,7 +15,7 @@ class BootStrap {
 					log "Made it to prompt!" 
 					block {
 						prompt(bargein: "false", timeout: "8000ms", "xml:lang": "en-US") {
-							audio(src: "/ivr-survey/prompt/fetch/${promptInstance.id}", promptInstance.text)	
+							audio(src: "/${app-name}/prompt/fetch/${promptInstance.id}", promptInstance.text)	
 						}
 					}
 					block {
@@ -33,19 +33,19 @@ class BootStrap {
 				form("", id: "captureResponse") {
 					"field"("", name: "surveyInput") {
 						prompt(bargein: "false", timeout: "8000ms") {
-							audio(src: "/ivr-survey/prompt/fetch/${questionInstance.id}", questionInstance.text)
+							audio(src: "/${app.name}/prompt/fetch/${questionInstance.id}", questionInstance.text)
 						}
 						grammars.each {
 							grammar(mode: "${it.mode}", "xml:lang": "en-US", maxage: "0", weight: "1.0", src: "/ivr-survey/grammar/fetch/${it.id}", type: "application/srgs+xml")
 						}
 					}
 					noinput {
-						audio(src: "/ivr-survey/message/fetch/${session?.noInput}")
+						audio(src: "/${app.name}/message/fetch/${session?.noInput}")
 						prompt("I did not understand your response.")
 						reprompt()
 					}
 					nomatch { 
-						audio(src: "/ivr-survey/message/fetch/${session?.noMatch}")
+						audio(src: "/${app.name}/message/fetch/${session?.noMatch}")
 						prompt("Your input is invalid.")
 						reprompt()
 					}
